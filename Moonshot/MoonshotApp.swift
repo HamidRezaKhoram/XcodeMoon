7fe5fac06591fce905afe0de4edc0404435e5623
hamidreza <hamidrezakhoramrokh@gmail.com>
2022-08-15 15:07:21 +0430

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Hamid on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
